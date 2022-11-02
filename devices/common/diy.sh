cc0f06f7237307687bbb362663c44f662179ac4c
ilxp <61609258+ilxp@users.noreply.github.com>
2022-11-02 16:36:16 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -174,5 +174,5 @@ sed -i '$a src-git diy https://github.com/xiaorouji/openwrt-passwall' feeds.conf
 
 # 内核显示增加自己个性名称(21.3.2 %y : 年份的最后两位数字)
 date=`date +%y.%m.%d`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='OneWRT N-EOL-%C From Lienol Openwrt %V'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='OneWRT N-EOL-%C From Lienol OpenWRT %V'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
