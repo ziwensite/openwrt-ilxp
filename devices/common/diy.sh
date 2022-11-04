c2da4d90151fe1f8ee2aa4ed4b8055a19dceddb5
ilxp <61609258+ilxp@users.noreply.github.com>
2022-11-05 06:02:50 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -75,7 +75,7 @@ rm -Rf feeds/luci/transplant/luci-app-nft-qos
 #svn export --force https://github.com/immortalwrt/packages/trunk/net/adguardhome package/diy/adguardhome
 
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/diy/luci-app-adguardhome
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/diy/adguardhome
+
 
 #svn export --force https://github.com/x-wrt/packages/trunk/net/nft-qos  package/diy/nft-qos
 #svn export --force https://github.com/x-wrt/luci/trunk/applications/luci-app-nft-qos package/diy/luci-app-nft-qos无法使用
