05c9bc4b7ba49481751be4e467d815b987dd5ff4
ilxp <61609258+ilxp@users.noreply.github.com>
2022-11-09 08:24:03 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -6,7 +6,7 @@
 #sed -i 's/OpenWrt/OneWrt/g' package/base-files/files/bin/config_generate
 #添加img编译时间前缀。
 #sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d)-OneWRT-N1907-/g' include/image.mk
-sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=$(shell date +%Y%m%d)-OneWRT-1907-EOL/g' include/image.mk
+sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=$(shell date +%Y%m%d)-OpoK-1907-EOL/g' include/image.mk
 # 修改内核版本
 #sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
 
